fc08bc153a6fb9c202f66c6de0d275fc55adf574
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-10-30 21:45:59 +0600

    user resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_URI),
     BookModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
