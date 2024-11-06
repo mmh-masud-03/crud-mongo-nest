ec86c2a7fa6ae340092ed9508bbf899b1512f302
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-11-07 00:33:04 +0600

    array of roles added during jwt sign

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [UserModule, JwtModule.register({ secret: 'secret key' })],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'secret key', // Ensure this matches the key used to sign the JWT
+      signOptions: { expiresIn: '1h' }, // Optional: specify token expiration time
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
